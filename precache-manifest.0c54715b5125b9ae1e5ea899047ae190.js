67b11a74c6d8dce4242711252239ef6a5dccb7e4
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-07-31 18:51:02 +0530

    Updates

---
+++ precache-manifest.0c54715b5125b9ae1e5ea899047ae190.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0268275c7f7a877b40796382c36c8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "32028243dd697c0399fe",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32028243dd697c0399fe",
+    "url": "/static/js/main.e5ad5d3a.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/static/media/resume.0c246fa0.pdf"
+  }
+]);
